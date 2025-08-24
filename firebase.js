b1742eb7d8cee6a4abeca7a0a8c1205557e760f0
MALWSOFTER <gevayvazyan365@gmail.com>
2025-08-24 14:38:46 +0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBK47x2qAkDioP4qQa5hManG9ULuNAp7dI",
+  authDomain: "now-fire-e0c41.firebaseapp.com",
+  databaseURL: "https://now-fire-e0c41-default-rtdb.firebaseio.com/",
+  projectId: "now-fire-e0c41",
+  storageBucket: "now-fire-e0c41.firebasestorage.app",
+  messagingSenderId: "772613763929",
+  appId: "1:772613763929:web:d76eda394b2f3b60d49248",
+  measurementId: "G-3PCY7N3FQJ"
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
